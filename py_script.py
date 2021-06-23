3d0becdf501e494579d59f6d60d7ab463c912d8b
Matthew Farant <matthewfarant@gmail.com>
2021-06-23 21:04:12 +0700

    commit

--- py_script.py
+++ py_script.py
@@ -3,3 +3,4 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import textblob as tb
+
--- r_script.R
+++ r_script.R
@@ -4,3 +4,6 @@ library(tidytext)
 library(textclean)
 library(stringr)
 library(rtweet)
+
+tweet_faisal<-get_timeline(user='@faisalbasri',n=3200)
+View(tweet_faisal)
