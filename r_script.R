58f75af2e6bbc8b580c94bfa91f36854da7cd420
Matthew Farant <matthewfarant@gmail.com>
2021-06-23 21:06:34 +0700

    clean @faisalbasri's tweets

--- r_script.R
+++ r_script.R
@@ -7,3 +7,16 @@ library(rtweet)
 
 tweet_faisal<-get_timeline(user='@faisalbasri',n=3200)
 View(tweet_faisal)
+
+kpk_faisal<-tweet_faisal %>% 
+  filter(grepl('KPK',text,ignore.case = T))
+
+kpk_faisal$text %>% 
+  str_to_lower() %>% 
+  replace_contraction() %>% 
+  replace_symbol() %>% 
+  replace_url() %>% 
+  replace_emoji() %>% 
+  strip()->tweets_faisal
+
+write.csv(tweets_faisal,'tweets_faisal.csv')
